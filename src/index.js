fbea6ca32bc2c6da9eb9669a58414c9b8dcd3f05
SASHAFAME <sadsparrow@mail.ru>
2022-07-12 23:08:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  n = String(n);
+  let splitString = n.split("");
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 }
